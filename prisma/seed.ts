7ccf35b23ea31ae60e8ccd2a8bc73e3b9691d4c1
LynchHarrison <hblynch2001@gmail.com>
2023-10-18 10:06:30 -0700

    added seed file - made adjustments to schema for seed to work - added/adjusted script to reset database for reseeding

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,39 +1,159 @@
+import { Prisma } from '@prisma/client';
 import prisma from '../lib/prisma'
 
 async function main() {
-  const response = await Promise.all([
-    prisma.users.upsert({
-      where: { email: 'rauchg@vercel.com' },
-      update: {},
-      create: {
-        name: 'Guillermo Rauch',
-        email: 'rauchg@vercel.com',
-        image:
-          'https://pbs.twimg.com/profile_images/1576257734810312704/ucxb4lHy_400x400.jpg',
+// These are variables for the transactions
+// - Will allow us to have an Open, Active, and Completed transaction
+// One week ago
+const oneWeekAgoStartDate = new Date();
+oneWeekAgoStartDate.setDate(oneWeekAgoStartDate.getDate() - 7);
+const oneWeekAgoEndDate = new Date();
+oneWeekAgoEndDate.setDate(oneWeekAgoEndDate.getDate() - 6);
+
+// Started today and ends in 2 days
+const today = new Date();
+const twoDaysLater = new Date();
+twoDaysLater.setDate(today.getDate() + 2);
+
+// In 1 week 
+const oneWeekLater = new Date();
+oneWeekLater.setDate(today.getDate() + 7);
+const twoDaysAfterOneWeek = new Date();
+twoDaysAfterOneWeek.setDate(oneWeekLater.getDate() + 2);
+
+  const user1 = await prisma.user.create({
+    data: {
+      name: 'James',
+      email: 'james@raw.com',
+      username: 'james',
+      password: '123',
+    },
+  });
+
+  const user2 = await prisma.user.create({
+    data: {
+      name: 'Sean',
+      email: 'sean@raw.com',
+      username: 'sean',
+      password: '123',
+    },
+  });
+
+  const user3 = await prisma.user.create({
+    data: {
+      name: 'Yong',
+      email: 'yong@raw.com',
+      username: 'yong',
+      password: '123',
+    },
+  });
+
+  const user4 = await prisma.user.create({
+    data: {
+      name: 'Harrison',
+      email: 'harrison@raw.com',
+      username: 'harrison',
+      password: '123',
+    },
+  });
+
+  const locationAustin = await prisma.location.create({
+    data: {
+      name: 'Austin, Texas',
+      latitude: 30.2672,
+      longitude: -97.7431,
+    },
+  });
+
+  const item1 = await prisma.item.create({
+    data: {
+      name: 'Hammer',
+      description: 'Bang on wall - I hit nails, I try not to hit thumbs... But sometimes I do try!',
+      price: 2,
+      owner: {
+        connect: { id: user1.id },
       },
-    }),
-    prisma.users.upsert({
-      where: { email: 'lee@vercel.com' },
-      update: {},
-      create: {
-        name: 'Lee Robinson',
-        email: 'lee@vercel.com',
-        image:
-          'https://pbs.twimg.com/profile_images/1587647097670467584/adWRdqQ6_400x400.jpg',
+      location: {
+        connect: { id: locationAustin.id },
       },
-    }),
-    await prisma.users.upsert({
-      where: { email: 'stey@vercel.com' },
-      update: {},
-      create: {
-        name: 'Steven Tey',
-        email: 'stey@vercel.com',
-        image:
-          'https://pbs.twimg.com/profile_images/1506792347840888834/dS-r50Je_400x400.jpg',
+    },
+  });
+
+  await prisma.review.create({
+    data: {
+      rating: 4,
+      text: 'It was a hammer that nailed it. Grip was meh though',
+      reviewer: {
+        connect: { id: user2.id },
       },
-    }),
-  ])
-  console.log(response)
+      item: {
+        connect: { id: item1.id },
+      },
+    },
+  });
+
+  await prisma.transaction.create({
+    data: {
+      status: 'COMPLETED',
+      renter: {
+        connect: { id: user2.id },
+      },
+      item: {
+        connect: { id: item1.id },
+      },
+      startDate: oneWeekAgoStartDate,
+      endDate: oneWeekAgoEndDate,
+      fee: 4,
+    },
+  });
+  await prisma.transaction.create({
+    data: {
+      status: 'ACTIVE',
+      renter: {
+        connect: { id: user2.id },
+      },
+      item: {
+        connect: { id: item1.id },
+      },
+      startDate: today,
+      endDate: twoDaysLater,
+      fee: 4,
+    },
+  });
+  await prisma.transaction.create({
+    data: {
+      status: 'OPEN',
+      renter: {
+        connect: { id: user2.id },
+      },
+      item: {
+        connect: { id: item1.id },
+      },
+      startDate: oneWeekLater,
+      endDate: twoDaysAfterOneWeek,
+      fee: 4,
+    },
+  });
+
+  await prisma.category.create({
+    data: {
+      name: 'hand tool',
+      items: {
+        connect: { id: item1.id }
+      }
+    }
+  })
+
+  await prisma.image.create({
+    data: {
+      url: 'https://www.harborfreight.com/media/catalog/product/cache/9fc4a8332f9638515cd199dd0f9238da/6/7/67716_W3.jpg',
+      item: {
+        connect: { id: item1.id}
+      }
+    }
+  })
+
+  console.log('the data is seeded!')
 }
 main()
   .then(async () => {
