08801092c009a9918e255bf2542ceff164abfec6
Sean Paulson <61070403+SeanPaulson@users.noreply.github.com>
2023-10-19 07:28:19 -0600

    updated tailwind font style

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,12 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Lexend_Deca } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Lexend_Deca({
+  subsets: ['latin'],
+  weight: ['100', '300', '400', '500', '600', '700'],
+  variable: '--font-lexend-Deca'
+})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.variable} font-sans`}>{children}</body>
     </html>
   )
 }
