4ac347a67ddd48c2011f81df19f01abdf0e21a3a
James Lee <jamesleex86@gmail.com>
2023-10-20 11:56:37 -0700

    basic navbar complete - without any data from backend

---
+++ app/components/NavBar/navbar.tsx
@@ -0,0 +1,46 @@
+import Image from 'next/image';
+import logo from '../icons/logo.png';
+import menu from '../icons/menu.png';
+import shoppingCart from '../icons/shopping-cart.png';
+import magnifyingGlass from '../icons/magnifying-glass.png';
+import s from "./navbar.module.css";
+
+
+const NavBar: React.FC = () => {
+
+    return (
+        <div className={s.navbarContainer}>
+            <div className={s.menuContainer}>
+                <Image src={menu} className={s.menuIcon} alt="" />
+            </div>
+            <div className={s.homeAboutContact}>
+                <div className={s.home}>
+                    Home
+                </div>
+                <div className={s.about}>
+                    About
+                </div>
+                <div className={s.contact}>
+                    Contact
+                </div>
+            </div>
+            <div className={s.logoContainer}>
+                <Image src={logo} className={s.logo} alt="" />
+            </div>
+            <div className={s.searchBarContainer}>
+                <div className={s.searchBar}>
+                    <div className={s.searchIcon}>
+                        <div><Image src={magnifyingGlass} className={s.magnifyingGlassIcon} alt="" /></div>
+                        <div>Search for tools</div>
+                    </div>
+                </div>
+            </div>
+            <div className={s.shoppingCartContainer}>
+                <Image src={shoppingCart} className={s.shoppingCartIcon} alt="" />
+            </div>
+        </div>
+    )
+};
+
+
+export default NavBar;
