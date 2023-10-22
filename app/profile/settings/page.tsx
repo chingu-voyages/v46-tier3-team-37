44a8180313f565e438a09a4e262260bad7d404f3
Sean Paulson <61070403+SeanPaulson@users.noreply.github.com>
2023-10-22 09:31:36 -0600

    profile/settings responsive stylings

--- app/profile/settings/page.tsx
+++ app/profile/settings/page.tsx
@@ -3,19 +3,19 @@ import Image from "next/image";
 
 export default function Settings() {
     return (
-        <div className="h-screen flex items-center flex-col mt-10">
+        <div className="h-screen flex items-center lg:px-40 flex-col mt-10">
             <section aria-labelledby="User Info" className="w-3/4 text-center ">
                 <h4 className="border-b-2 border-b-[lightgray] dark:border-b-[#3d3d3d]">User Info</h4>
-                <ul className="my-10">
-                    <li className="flex justify-between my-10">
+                <ul className="my-10 md:mx-32 lg:mx-0">
+                    <li className="flex lg:justify-around justify-between my-10">
                         <Image src={'/facebook.png'} height={30} width={30} alt='facebook logo' />
                         <Button variant={'default'} size={'md'}>Logout</Button>
                     </li>
-                    <li className="flex justify-between my-10">
+                    <li className="flex lg:justify-around justify-between my-10">
                         <Image src={'/facebook.png'} height={30} width={30} alt='facebook logo' />
                         <Button variant={'default'} size={'md'}>Logout</Button>
                     </li>
-                    <li className="flex justify-between my-10">
+                    <li className="flex lg:justify-around justify-between my-10">
                         <Image src={'/facebook.png'} height={30} width={30} alt='facebook logo' />
                         <Button variant={'default'} size={'md'}>Logout</Button>
                     </li>
@@ -23,12 +23,12 @@ export default function Settings() {
             </section>
             <section aria-labelledby="User Info" className="w-3/4 text-center ">
                 <h4 className="border-b-2 border-b-[lightgray] dark:border-b-[#3d3d3d]">User Credentials</h4>
-                <ul className="my-10">
-                    <li className="flex justify-between my-10">
+                <ul className="my-10 md:mx-32 lg:bg-blue">
+                    <li className="flex lg:justify-around justify-between my-10">
                         <Image src={'/facebook.png'} height={30} width={30} alt='facebook logo' />
                         <Button className="basis-1/2" variant={'thin'} size={'sm'}>change email</Button>
                     </li>
-                    <li className="flex justify-between my-10">
+                    <li className="flex lg:justify-around justify-between my-10">
                         <Image src={'/facebook.png'} height={30} width={30} alt='facebook logo' />
                         <Button className="basis-1/2" variant={'thin'} size={'sm'}>change password</Button>
                     </li>
