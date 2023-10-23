d9ef4dd705fa97016ed3a7a71c8d6bb07224bc02
Sean Paulson <61070403+SeanPaulson@users.noreply.github.com>
2023-10-23 09:11:53 -0600

    fixed some UI bugs in the Card.tsx and Button.tsx components

--- components/uiComponents/CardFooter.tsx
+++ components/uiComponents/CardFooter.tsx
@@ -31,30 +31,28 @@ interface CardFooterVariantProps
 
 export default function CardFooter({ variant, parentVariant, size, price, children, show }: CardFooterVariantProps) {
 
-    let detailedBtn: React.ReactNode | React.ReactNode[] = children;
-    let defaultBtn: React.ReactNode = children;
+    let btnChildren: React.ReactNode | React.ReactNode[];;
 
     React.Children.toArray(children)
     React.Children.forEach(children, (child, index) => {
         if (!React.isValidElement(child)) return;
-        if (child.type === 'button' && parentVariant === 'default' && index === 0) {
-            defaultBtn = child
+        if (child.type === 'button') {
+            btnChildren = children;
         } else { return }
     })
-    console.log(show)
     return (
         <div className={CardFooterVariants({ variant, size })}>
             {
                  show && parentVariant === 'detailed' ?
-                    <div className={`bg-[white] flex flex-col w-full gap-4 shadow-md dark:bg-foregroundPrimary font-normal text-sm px-5 py-4`}>
+                    <div className={`bg-[#f6f6f6] flex flex-col w-full gap-4 dark:bg-foregroundPrimary font-normal text-sm px-5 py-4`}>
                         <span>
                             <p><b>price:</b> {price}</p>
                             <p>other details TODO</p>
                         </span>
-                        <div className="flex justify-end gap-3">{React.Children.map(detailedBtn, (child) => <>{child}</>)}</div>
+                        <div className="flex justify-end gap-3">{React.Children.map(btnChildren, (child) => <>{child}</>)}</div>
                     </div>
                     :
-                    <>{defaultBtn}</>
+                    <>{btnChildren}</>
             }
         </div>
     )
