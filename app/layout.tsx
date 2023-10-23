c0aca2398cf6aebba036feda6923b9922fcddac7
James Lee <jamesleex86@gmail.com>
2023-10-23 08:17:35 -0700

    Changed components file name

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import NavBar from './components/NavBar/navbar'
+import NavBar from './components2/NavBar/navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
