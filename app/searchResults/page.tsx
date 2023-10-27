541e69ecc7da94ef1f96939b56a9458129e9ee52
James Lee <jamesleex86@gmail.com>
2023-10-27 13:10:06 -0700

    Added route for item to specific item page

--- app/searchResults/page.tsx
+++ app/searchResults/page.tsx
@@ -1,6 +1,7 @@
 "use client"
-import { useSearchParams } from 'next/navigation';
 import React, { useEffect, useState } from 'react';
+import { useSearchParams } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 import Button from "@/components/uiComponents/Button";
 import Card from "@/components/uiComponents/Card";
 import { Tool } from "@/types/schemaTypes";
@@ -8,6 +9,7 @@ import s from "./page.module.css";
 
 
 const SearchResults: React.FC = () => {
+    const router = useRouter();
     const searchParams = useSearchParams();
     const toolName = searchParams.get('toolName');
 
@@ -15,8 +17,6 @@ const SearchResults: React.FC = () => {
     const [isLoading, setLoading] = React.useState<boolean>(true);
 
     let toolsByName: Tool[] = [];
-
-    console.log('toolsData', toolsData)
 
     if (toolName) {
         toolsByName = toolsData
@@ -32,6 +32,10 @@ const SearchResults: React.FC = () => {
             })
     }, []);
 
+    const routeToItemPage = (toolId: string) => {
+        router.push(`/tools/${toolId}`);
+    }
+
     return (
         <div className={s.searchResultsContainer}>
             {toolsByName.length > 0
@@ -41,8 +45,8 @@ const SearchResults: React.FC = () => {
                     {toolsByName.map(tool => (
                         <div className="flex justify-center mt-4" key={tool.id}>
                             <div className="flex gap-2 flex-col md:w-1/2">
-                                <div key={tool.id}>
-                                    <Card active={true} variant={'detailed'} title={tool.name} description={tool.description} price={tool.price.toString()}>
+                                <div onClick={() => routeToItemPage(tool.id)} key={tool.id}>
+                                    <Card size={'md'} active={true} variant={'default'} title={tool.name} description={tool.description} price={tool.price.toString()} >
                                         <Button variant={'thin'} size={'sm'} cardType='detailed'>More info</Button>
                                     </Card>
                                 </div>
