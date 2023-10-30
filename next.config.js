1a2d36e171a9f379ee7c8e609c0646b980e3ae93
Sean Paulson <61070403+SeanPaulson@users.noreply.github.com>
2023-10-30 13:41:11 -0600

    added server actions to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverActions: true
+  },
     images: {
     domains: ['media.licdn.com', 'pbs.twimg.com', 'www.harborfreight.com'], // Add the hostname(s) you want to allow
   }
