a3dcf463df91784443aaace7f7679aafbd442728
Yong Cho <yong-cho@outlook.com>
2023-10-31 11:25:31 -0500

    DRY AuthProvider.tsx code

--- context/AuthProvider.tsx
+++ context/AuthProvider.tsx
@@ -1,14 +1,4 @@
 'use client'
-
 import { SessionProvider } from 'next-auth/react'
-import React from 'react'
 
-export default function AuthProvider({
-  children,
-  session,
-}: {
-  children: React.ReactNode
-  session: any
-}) {
-  return <SessionProvider session={session}>{children}</SessionProvider>
-}
+export default SessionProvider
