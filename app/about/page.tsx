3e391e1e64d53337d62b8dd40c89b5ffc289ee38
Yong Cho <yong-cho@outlook.com>
2023-10-31 14:45:26 -0500

    Add techstack to the about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,49 @@
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
-import Postgres from '@/components/icons/postgresql.svg'
+import Postgres from '@/public/postgresql.svg'
+import NextJS from '@/public/nextjs.svg'
+import Github from '@/public/github.svg'
+import Prisma from '@/public/prisma.svg'
+import ReactIcon from '@/public/react.svg'
+import Tailwind from '@/public/tailwind.svg'
+import Supabase from '@/public/supabase.svg'
+import TypeScript from '@/public/typescript.svg'
+
+const techStack = [
+  {
+    name: 'Postgres',
+    url: Postgres,
+  },
+  {
+    name: 'NextJS',
+    url: NextJS,
+  },
+  {
+    name: 'Tailwind',
+    url: Tailwind,
+  },
+  {
+    name: 'React',
+    url: ReactIcon,
+  },
+  {
+    name: 'Supabase',
+    url: Supabase,
+  },
+  {
+    name: 'Github',
+    url: Github,
+  },
+  {
+    name: 'Prisma',
+    url: Prisma,
+  },
+  {
+    name: 'TypeScript',
+    url: TypeScript,
+  },
+]
 
 const About = () => {
   return (
@@ -17,61 +59,70 @@ const About = () => {
       </div>
       <div className='grid grid-cols-1 gap-6 md:grid-cols-2'>
         <div className='flex flex-col w-[240px]'>
-          <Image
-            src='https://media.licdn.com/dms/image/D5635AQEYQuydKj_FNg/profile-framedphoto-shrink_800_800/0/1681751278762?e=1698962400&v=beta&t=5XJgJ09QZdcvU0gyfblsoLcAXBx7oqVz1b2QUD1zY70'
-            width={240}
-            height={240}
-            className='mb-4 rounded-md'
-            alt='yong-image'
-          />
+          <Link href='https://www.linkedin.com/in/lynchharrison/'>
+            <Image
+              src='https://media.licdn.com/dms/image/D5603AQEFQ9bPMTJ2Yw/profile-displayphoto-shrink_800_800/0/1686068486077?e=1703721600&v=beta&t=eh1PM9yuBTXKrlLBFtOqQcnQGOV5UpUnaHWqIQ1bO3o'
+              width={240}
+              height={240}
+              className='mb-4 rounded-md'
+              alt='harrison-image'
+            />
+          </Link>
           <div>
             <p className='mb-4 text-[#D3CAB0]'>
-              Meet Yong, who enjoys making useful apps, fun apps, learning new
-              tech stack, and collaborating with others to create apps.
+              Meet Harrison, a backend guru who specializes in database schema
+              modeling and technical project management.
             </p>
           </div>
         </div>
         <div className='flex flex-col w-[240px]'>
-          <Image
-            src='https://media.licdn.com/dms/image/D5603AQFoLCSGAugU1Q/profile-displayphoto-shrink_800_800/0/1681922763972?e=1703721600&v=beta&t=HC3vlaQ1f01jtfOeqf25CpxC8vWAanr7szC-KTvRQ6k'
-            width={240}
-            height={240}
-            className='mb-4 rounded-md'
-            alt='james-image'
-          />
+          <Link href='https://www.linkedin.com/in/jameswonlee/'>
+            <Image
+              src='https://media.licdn.com/dms/image/D5603AQFoLCSGAugU1Q/profile-displayphoto-shrink_800_800/0/1681922763972?e=1703721600&v=beta&t=HC3vlaQ1f01jtfOeqf25CpxC8vWAanr7szC-KTvRQ6k'
+              width={240}
+              height={240}
+              className='mb-4 rounded-md'
+              alt='james-image'
+            />
+          </Link>
           <div>
             <p className='mb-4 text-[#D3CAB0]'>
-              Meet James, who enjoys making useful apps, fun apps, learning new
-              tech stack, and collaborating with others to create apps.
+              Meet James, who specializes in logo making and frontend UI
+              development.
             </p>
           </div>
         </div>
         <div className='flex flex-col w-[240px]'>
-          <Image
-            src='https://media.licdn.com/dms/image/C5603AQGzoEpo72qRUw/profile-displayphoto-shrink_800_800/0/1650313047528?e=1703721600&v=beta&t=E1A7KRtTBKZW35LxayCPon8j2CwbVvMIhgIF7dzlpWc'
-            width={240}
-            height={240}
-            className='mb-4 rounded-md'
-            alt='sean-image'
-          />
+          <Link href='https://www.linkedin.com/in/yong-cho-dev/'>
+            <Image
+              src='https://media.licdn.com/dms/image/D5635AQEYQuydKj_FNg/profile-framedphoto-shrink_800_800/0/1681751278762?e=1698962400&v=beta&t=5XJgJ09QZdcvU0gyfblsoLcAXBx7oqVz1b2QUD1zY70'
+              width={240}
+              height={240}
+              className='mb-4 rounded-md'
+              alt='yong-image'
+            />
+          </Link>
           <div>
             <p className='mb-4 text-[#D3CAB0]'>
-              Meet Sean, who is an expert at TS, figma, and resuable components.
+              Meet Yong, who enjoys making fun and useful apps, and
+              collaborating with others to create new apps.
             </p>
           </div>
         </div>
         <div className='flex flex-col w-[240px]'>
-          <Image
-            src='https://media.licdn.com/dms/image/D5603AQEFQ9bPMTJ2Yw/profile-displayphoto-shrink_800_800/0/1686068486077?e=1703721600&v=beta&t=eh1PM9yuBTXKrlLBFtOqQcnQGOV5UpUnaHWqIQ1bO3o'
-            width={240}
-            height={240}
-            className='mb-4 rounded-md'
-            alt='harrison-image'
-          />
+          <Link href='https://linkedin.com/in/sean-paulson-64b519225/'>
+            <Image
+              src='https://media.licdn.com/dms/image/C5603AQGzoEpo72qRUw/profile-displayphoto-shrink_800_800/0/1650313047528?e=1703721600&v=beta&t=E1A7KRtTBKZW35LxayCPon8j2CwbVvMIhgIF7dzlpWc'
+              width={240}
+              height={240}
+              className='mb-4 rounded-md'
+              alt='sean-image'
+            />
+          </Link>
           <div>
             <p className='mb-4 text-[#D3CAB0]'>
-              Meet Harrison, who enjoys making useful apps, fun apps, learning
-              new tech stack, and collaborating with others to create apps.
+              Meet Sean, who is an expert at TypeScript, Figma design, and
+              creating resuable components.
             </p>
           </div>
         </div>
@@ -85,78 +136,22 @@ const About = () => {
         </p>
       </div>
       <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
-        <div className='m-4 hover:scale-125'>
-          <Image
-            width={150}
-            alt='postgres-icon'
-            src={Postgres}
-            className='w-20 mx-auto mt-5'
-          />
-          <p className='my-4  text-[#D3CAB0]'>PostgreSQL</p>
-        </div>
-        <div className='m-4 hover:scale-125'>
-          <Image
-            width={150}
-            alt='postgres-icon'
-            src={Postgres}
-            className='w-20 mx-auto mt-5'
-          />
-          <p className='my-4 text-[#D3CAB0]'>PostgreSQL</p>
-        </div>
-        <div className='m-4 hover:scale-125'>
-          <Image
-            width={150}
-            alt='postgres-icon'
-            src={Postgres}
-            className='w-20 mx-auto mt-5'
-          />
-          <p className='my-4 text-[#D3CAB0]'>PostgreSQL</p>
-        </div>
-        <div className='m-4 hover:scale-125'>
-          <Image
-            width={150}
-            alt='postgres-icon'
-            src={Postgres}
-            className='w-20 mx-auto mt-5'
-          />
-          <p className='my-4 text-[#D3CAB0]'>PostgreSQL</p>
-        </div>
-        <div className='m-4 hover:scale-125'>
-          <Image
-            width={150}
-            alt='postgres-icon'
-            src={Postgres}
-            className='w-20 mx-auto mt-5'
-          />
-          <p className='my-4 text-[#D3CAB0]'>PostgreSQL</p>
-        </div>
-        <div className='m-4 hover:scale-125'>
-          <Image
-            width={150}
-            alt='postgres-icon'
-            src={Postgres}
-            className='w-20 mx-auto mt-5'
-          />
-          <p className='my-4 text-[#D3CAB0]'>PostgreSQL</p>
-        </div>
-        <div className='m-4 hover:scale-125'>
-          <Image
-            width={150}
-            alt='postgres-icon'
-            src={Postgres}
-            className='w-20 mx-auto mt-5'
-          />
-          <p className='my-4 text-[#D3CAB0]'>PostgreSQL</p>
-        </div>
-        <div className='m-4 hover:scale-125'>
-          <Image
-            width={150}
-            alt='postgres-icon'
-            src={Postgres}
-            className='w-20 mx-auto mt-5'
-          />
-          <p className='my-4 text-[#D3CAB0]'>PostgreSQL</p>
-        </div>
+        {techStack &&
+          techStack.map((stack, i) => {
+            console.log(stack.url)
+            return (
+              <div className='m-4 hover:scale-125'>
+                <Image
+                  width={150}
+                  height={150}
+                  alt={`${stack.name}-icon`}
+                  src={stack.url}
+                  className='w-20 mx-auto mt-5'
+                />
+                <p className='my-4 text-[#D3CAB0]'>{stack.name}</p>
+              </div>
+            )
+          })}
       </div>
     </div>
   )
