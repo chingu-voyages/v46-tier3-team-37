d5e82e9adb6ea6bb64a85187a418ea2c8efa1b8b
Sean Paulson <61070403+SeanPaulson@users.noreply.github.com>
2023-11-01 14:30:17 -0600

    New listings Post feature

--- app/profile/listings/newListing/CreateListingForm.tsx
+++ app/profile/listings/newListing/CreateListingForm.tsx
@@ -1,35 +1,67 @@
+import { options } from "@/app/api/auth/[...nextauth]/options";
 import Button from "@/components/uiComponents/Button";
+import { getServerSession } from "next-auth";
+import prisma from "@/lib/prisma";
+import { redirect } from "next/navigation";
 
 
-export default function () {
+export default async function () {
+
+    const session = await getServerSession(options);
+    if (!session) redirect('/login')
+
+    async function createListing(data: FormData) {
+        "use server";
+        if (!session) {
+            return
+        }
+        console.log(session.user)
+        const newListing = await prisma.item.create({
+            data: {
+                name: data.get('name')?.toString() ?? '',
+                description: data.get('description')?.toString() ?? '',
+                price: Number(data.get('price')) ?? '',
+                owner: {
+                    connect: { 'username': session.user.username },
+                },
+                location: {
+                    connect: { id: 'clo95y7ua00046xgfyv0ccp0a' },
+                },
+            },
+        })
+        if (newListing) {
+            redirect('/profile')
+        }
+    }
+
     return (
-        <form className="flex flex-col gap-12 items-center m-auto md:w-1/2 w-100">
+        <form action={createListing} className="flex flex-col gap-12 items-center m-auto md:w-1/2 w-100">
             <ul className="flex flex-col w-full gap-5">
                 <li className="flex justify-evenly gap-4">
                     <span className="flex flex-col">
                         <label className="text-left text-sm font-light text-[black] dark:text-fontPrimary" htmlFor="name">Name</label>
-                        <input type="text" id="name" name="name" placeholder="Name:" />
+                        <input required className='text-[black]' type="text" id="name" name="name" placeholder="Name:" />
                     </span>
                     <span className="flex flex-col">
-                        <label className="text-left text-sm font-light text-[black] dark:text-fontPrimary" htmlFor="name">Location</label>
-                        <input type="text" value={'Texas'} id="name" name="name" />
+                        <label className="text-left text-sm font-light text-[black] dark:text-fontPrimary" htmlFor="Location">Location</label>
+                        <input className='text-[black]' type="text" value='Texas' readOnly id="Location" name="Location" />
                     </span>
                 </li>
                 <li className="flex justify-evenly gap-4">
                     <span className="flex flex-col">
                         <label className="text-left text-sm font-light text-[black] dark:text-fontPrimary" htmlFor="description">description</label>
-                        <input type="text" id="description" name="description" placeholder="description:" />
+                        <input required className='text-[black]' type="text" id="description" name="description" placeholder="description:" />
                     </span>
                     <span className="flex flex-col">
                         <label className="text-left text-sm font-light text-[black] dark:text-fontPrimary" htmlFor="price">cost/day</label>
-                        <input type="text" placeholder="$00.00" id="price" name="price" />
+                        <input required className='text-[black]' type="number" placeholder="$00.00" id="price" name="price" />
                     </span>
                 </li>
                 <li className="flex justify-between gap-4">
                     {/* show images? */}
                     <span className="flex flex-col">
                         <label className="text-left text-sm font-light text-[black] dark:text-fontPrimary" htmlFor="image">images</label>
-                        <input type="text" id="image" name="image" placeholder="image:" />
+                        <input className='text-[black]' type="text" id="image" name="image" placeholder="image:" />
                     </span>
                     <span className="flex items-center ">
                         <Button size='md'>Upload Images</Button>
--- app/profile/listings/newListing/page.tsx
+++ app/profile/listings/newListing/page.tsx
@@ -1,6 +1,9 @@
+import { getServerSession } from "next-auth";
 import CreateListingForm from "./CreateListingForm";
+import { options } from "@/app/api/auth/[...nextauth]/options";
 
-export default function () {
+export default async function () {
+    
     return (
         <section className="w-full my-8 text-center">
             <h1 className="font-bold my-12 text-2xl">List a new Item for Rent</h1>
