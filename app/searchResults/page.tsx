42779e87048da4f9e9905e5bb41624149d8633dd
James Lee <jamesleex86@gmail.com>
2023-11-03 11:49:22 -0700

    added isLoading state to searchResults

--- app/searchResults/page.tsx
+++ app/searchResults/page.tsx
@@ -71,51 +71,60 @@ const SearchResults: React.FC = () => {
     }
 
     return (
-        <div className={s.searchResultsContainer}>
-            {tools.length > 0
+        <section className={s.searchResults}>
+            {isLoading
                 ?
-                <div className={s.searchResults}>
-                    <div className={s.searchResultsTop}>
-                        {toolSearch !== ""
-                            ?
-                            <div>Search results for &apos;&apos;{toolSearch}&apos;&apos;</div>
-                            :
-                            <div>Displaying all tools:</div>
-                        }
-                        <div className={s.sortOptionsContiner}>
-                            <div className={s.sortOptions}>
-                                <div className={s.sortBy}>
-                                    <div>Sort by</div>
-                                </div>
-                                <select className={s.sortByOptions} onChange={e => {
-                                    setSortBy(e.target.value)
-                                }}>
-                                    <option value="price">Price</option>
-                                    <option value="name" selected={true}>Name</option>
-                                    <option value="available">Available now</option>
-                                </select>
-                            </div>
-                        </div>
-                    </div>
-                    {tools.map(tool => (
-                        <div className="flex justify-center mt-4" key={tool.id}>
-                            <div className="flex gap-2 flex-col md:w-1/2">
-                                <div onClick={() => routeToItemPage(tool.id)} key={tool.id}>
-                                    <Card size={'md'} active={true} variant={'default'}
-                                        title={tool.name}
-                                        description={tool.description}
-                                        price={tool.price.toString()}
-                                        imageSrc={tool.images.length ? tool.images[0].url : 'https://www.harborfreight.com/media/catalog/product/cache/9fc4a8332f9638515cd199dd0f9238da/6/7/67716_W3.jpg'}>
-                                    </Card>
-                                </div>
-                            </div>
-                        </div>
-                    ))}
+                <div>
+                    Searching for tools...
                 </div>
                 :
-                <div>No tools found for `{toolSearch}`</div>
+                <div className={s.searchResultsContainer}>
+                    {tools.length > 0
+                        ?
+                        <div className={s.searchResults}>
+                            <div className={s.searchResultsTop}>
+                                {toolSearch !== ""
+                                    ?
+                                    <div>Search results for &apos;&apos;{toolSearch}&apos;&apos;</div>
+                                    :
+                                    <div>Displaying all tools:</div>
+                                }
+                                <div className={s.sortOptionsContiner}>
+                                    <div className={s.sortOptions}>
+                                        <div className={s.sortBy}>
+                                            <div>Sort by</div>
+                                        </div>
+                                        <select className={s.sortByOptions} onChange={e => {
+                                            setSortBy(e.target.value)
+                                        }}>
+                                            <option value="price">Price</option>
+                                            <option value="name" selected={true}>Name</option>
+                                            <option value="available">Available now</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                            {tools.map(tool => (
+                                <div className="flex justify-center mt-4" key={tool.id}>
+                                    <div className="flex gap-2 flex-col md:w-1/2">
+                                        <div onClick={() => routeToItemPage(tool.id)} key={tool.id}>
+                                            <Card size={'md'} active={true} variant={'default'}
+                                                title={tool.name}
+                                                description={tool.description}
+                                                price={tool.price.toString()}
+                                                imageSrc={tool.images.length ? tool.images[0].url : 'https://www.harborfreight.com/media/catalog/product/cache/9fc4a8332f9638515cd199dd0f9238da/6/7/67716_W3.jpg'}>
+                                            </Card>
+                                        </div>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                        :
+                        <div>No tools found for `{toolSearch}`</div>
+                    }
+                </div>
             }
-        </div>
+        </section>
     )
 }
 
