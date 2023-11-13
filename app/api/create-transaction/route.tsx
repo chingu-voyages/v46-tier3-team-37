68642eb6b18c9006857268389124bfd9af51bc7b
LynchHarrison <hblynch2001@gmail.com>
2023-11-12 21:29:55 -0800

    removed method check for create transaction

--- app/api/create-transaction/route.tsx
+++ app/api/create-transaction/route.tsx
@@ -3,64 +3,51 @@ import prisma from '@/lib/prisma';
 
 export async function PUT(req: NextRequest) {
   try {
-    if (req.method === 'PUT') {
-      const contentType = req.headers.get('content-type');
-      if (contentType?.includes('application/json')) {
-        const body = await req.json();
-        const {
-          startDate,
-          endDate,
+    const contentType = req.headers.get('content-type');
+    if (contentType?.includes('application/json')) {
+      const body = await req.json();
+      const { startDate, endDate, itemId, renterId, fee } =
+        body;
+
+      const existingTransactions =
+        await prisma.transaction.findMany({
+          where: {
+            itemId,
+            OR: [
+              {
+                startDate: { lte: new Date(endDate) },
+                endDate: { gte: new Date(startDate) },
+              },
+              {
+                startDate: { lte: new Date(startDate) },
+                endDate: { gte: new Date(endDate) },
+              },
+            ],
+          },
+        });
+
+      if (existingTransactions.length > 0) {
+        return NextResponse.json({
+          error: 'Transaction dates are not available',
+        });
+      }
+
+      const transaction = await prisma.transaction.create({
+        data: {
+          startDate: new Date(startDate),
+          endDate: new Date(endDate),
           itemId,
           renterId,
           fee,
-        } = body;
+          status: 'HOLD',
+        },
+      });
 
-        const existingTransactions =
-          await prisma.transaction.findMany({
-            where: {
-              itemId,
-              OR: [
-                {
-                  startDate: { lte: new Date(endDate) },
-                  endDate: { gte: new Date(startDate) },
-                },
-                {
-                  startDate: { lte: new Date(startDate) },
-                  endDate: { gte: new Date(endDate) },
-                },
-              ],
-            },
-          });
-
-        if (existingTransactions.length > 0) {
-          return NextResponse.json({
-            error: 'Transaction dates are not available',
-          });
-        }
-
-        const transaction = await prisma.transaction.create(
-          {
-            data: {
-              startDate: new Date(startDate),
-              endDate: new Date(endDate),
-              itemId,
-              renterId,
-              fee,
-              status: 'HOLD',
-            },
-          }
-        );
-
-        return NextResponse.json({ transaction });
-      } else {
-        return NextResponse.json({
-          error:
-            'Invalid content type. Please provide JSON data.',
-        });
-      }
+      return NextResponse.json({ transaction });
     } else {
       return NextResponse.json({
-        error: 'This endpoint only accepts PUT requests.',
+        error:
+          'Invalid content type. Please provide JSON data.',
       });
     }
   } catch (error) {
