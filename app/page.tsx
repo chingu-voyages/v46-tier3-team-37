8ccc8b9069522162411244763343146f963912d0
Sean Paulson <61070403+SeanPaulson@users.noreply.github.com>
2023-11-13 13:23:15 -0700

    added links to item page from homepage cards

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Button from '@/components/uiComponents/Button'
 import Image from 'next/image'
 import { getFeaturedTools } from './actions';
 import HomePgForm from '@/components/forms/HomePgForm';
+import { Link } from 'react-router-dom';
 
 export default async function Home() {
 
@@ -17,25 +18,27 @@ export default async function Home() {
         <h1 className='font-bold text-center text-lg'>Find tools to rent here on Rent A Wrench!</h1>
         {/* <search by location and date form component will need to be split into a separate Component */}
         <div >
-         <HomePgForm />
+          <HomePgForm />
         </div>
       </section>
-        
+
       <section className='w-full lg:px-42 p-4 px-0 text-center'>
         {/* <Listings component will need to be split into a separate Component */}
         <h1 className='font-bold text-2xl mb-8'>Featured Listings</h1>
         <div className='flex justify-center gap-4 flex-wrap'>
           {featuredTools && featuredTools.map((tool) => (
-            <Card 
-            key={tool.id}
-            className='md:basis-1/4'
-            variant='default'
-            title={tool.name} 
-            size={'default'}
-            imageSrc={tool.images[0] && tool.images[0].url}
-            description={tool.description} 
+            <Card
+              key={tool.id}
+              className='md:basis-1/4'
+              variant='default'
+              title={tool.name}
+              size={'default'}
+              imageSrc={tool.images[0] && tool.images[0].url}
+              description={tool.description}
             >
-              <Button variant={'thin'} size={'sm'}>Details</Button>
+              <a href={`/tools/${tool.id}`}>
+                <Button variant={'thin'} size={'sm'}>Details</Button>
+              </a>
             </Card>
           ))}
         </div>
